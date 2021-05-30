32e12025a6c2c715e0b7a279c9ec775f912eccbb
Pedro da Conceicao <pedrocardo007@hotmail.com>
2021-05-29 22:15:31 -0300

    5 - Adicionado input time e funcionando

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,68 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Inputs = (props) => {
-    const [seg, setSeg] = useState(0)
-    const [min, setMin] = useState(0)
-    const [hor, setHor] = useState(0)
-
-    const limitSeg = (e) => {
-        if (e.target.value < 0){
-            setSeg(0)
-        } else if(e.target.value > 59){
-            setSeg(59);
-        } else if(e.target.value === ""){
-            setSeg(0);
-        } else {
-            setSeg(e.target.value)
-        }
-    }
-
-    const limitMin = (e) => {
-        if (e.target.value < 0){
-            setMin(0)
-        } else if(e.target.value > 59){
-            setMin(59);
-        } else if(e.target.value === ""){
-            setMin(0);
-        }else {
-            setMin(e.target.value);
-        }
-    }
-
-    const limitHor = (e) => {
-        if (e.target.value < 0){
-            setHor(0)
-        } else if(e.target.value > 24){
-            setHor(24);
-        } else if(e.target.value === ""){
-            setHor(0);
-        }else{
-            setHor(e.target.value);
-        }
-    }
-
-    const zerarTempo = () => {
-        setSeg(0)
-        setMin(0)
-        setHor(0)
-    }
 
     const pegarDados = (valores) => {
         valores.preventDefault();
-        if (seg !== "" && min !== "" && hor !== ""){
-            props.segundos(seg)
-            props.minutos(min)
-            props.horas(hor)
+        let dados = document.getElementById("tempo").value;
+        
+        if(dados !== ""){
+            let hora = parseInt(dados.slice(0,2));
+            let minuto = parseInt(dados.slice(3,5));
+            let segundo = parseInt(dados.slice(6,8));
 
-            zerarTempo();
+            props.segundos(segundo)
+            props.minutos(minuto)
+            props.horas(hora)
         }
     }
 
     return(
         <form>
-            <input type="number" value={hor} onChange={limitHor} />
-            <input type="number" value={min} onChange={limitMin} />
-            <input type="number" value={seg} onChange={limitSeg} />
+            <input
+                type="time"
+                step="3"
+                id="tempo" 
+                min="0"
+            />
             <button type="submit" onClick={pegarDados} id="botao-input4" >Adicionar</button>
         </form>
     )
